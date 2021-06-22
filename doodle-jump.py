bb09565fbfa415184c05cf4aabd45556be96018a
Maarten Wolfsen <maartenwolfsen@gmail.com>
2021-06-22 20:34:05 +0200

    Add Comments

--- doodle-jump.py
+++ doodle-jump.py
@@ -3,7 +3,7 @@ import neat
 import time
 import os
 import random
-import keyboard
+import pickle
 
 pygame.font.init()
 
@@ -17,6 +17,7 @@ JUMP_THRESHOLD = 210
 MAX_GENERATIONS = 1000
 MAX_PLATFORMS = 7
 MAX_WHILE = 30
+SCROLLING_VELOCITY = 5
 
 PLAYER_SPRITE_RIGHT = pygame.image.load(os.path.join("sprites", "player.png"))
 PLAYER_SPRITE_LEFT = pygame.transform.flip(PLAYER_SPRITE_RIGHT, True, False)
@@ -27,6 +28,7 @@ BG_SPRITE = pygame.image.load(os.path.join("sprites", "bg.png"))
 SCORE_FONT = pygame.font.SysFont("Verdana", 36)
 DEBUG_FONT = pygame.font.SysFont("Verdana", 14)
 
+# Player Class
 class Player:
     VELOCITY_X = 4
     VELOCITY_Y = 10
@@ -45,19 +47,20 @@ class Player:
         self.jump_animation_timer = 30
         self.vy = 0
 
+    # Move Player left
     def moveLeft(self):
         self.velocity_x = -self.VELOCITY_X
 
+    # Move Player right
     def moveRight(self):
         self.velocity_x = self.VELOCITY_X
 
-    def resetMove(self):
-        self.velocity_x = 0
-
+    # Jump by setting Y velocity to max
     def jump(self):
         self.velocity_y = -self.VELOCITY_Y
         self.jump_tick = 0
 
+    # Move Player based on X and Y velocity
     def move(self):
         self.jump_tick += self.JUMP_VELOCITY
 
@@ -73,6 +76,7 @@ class Player:
         self.y = self.y + vy
         self.x = self.x + self.velocity_x
 
+        # Jump animation
         if self.jump_tick < 7:
             if self.velocity_x >= 0:
                 self.image = PLAYER_JUMP_SPRITE_RIGHT
@@ -84,15 +88,18 @@ class Player:
             else:
                 self.image = PLAYER_SPRITE_LEFT
 
+        # Place Player on opposite wall when crossing window
         if self.x < 0 - (self.width / 2):
             self.x = WINDOW_WIDTH
 
         if self.x > WINDOW_WIDTH - (self.width / 2):
             self.x = 0 - (self.width / 2)
 
+    # Draw Player
     def draw(self, win):
         win.blit(self.image, (self.x, self.y))
 
+        # In Debug Mode, draw visible collision and coordinates
         if DEBUG_MODE:
             win.blit(
                 DEBUG_FONT.render(
@@ -108,6 +115,7 @@ class Player:
             surface.fill((0, 35, 255))
             win.blit(surface, (self.x + (self.width / 4), self.y + self.height))
 
+    # Check collision
     def collide(self, platforms):
         player_rect = pygame.Rect(
             self.x + (self.width / 4),
@@ -129,6 +137,7 @@ class Player:
 
         return False
 
+# Platform Class
 class Platform:
     VELOCITY = 5
 
@@ -137,21 +146,20 @@ class Platform:
         self.y = y
         self.width = PLATFORM_SPRITE.get_width()
         self.height = PLATFORM_SPRITE.get_height()
-
-        self.set_position()
-
-    def set_position(self):
         self.x = random.randrange(
             FIELD_MARGIN,
             WINDOW_WIDTH - self.width - FIELD_MARGIN
         )
 
+    # Move down (scroll)
     def move(self, y):
         self.y += y
 
+    # Draw Platform
     def draw(self, win):
         win.blit(PLATFORM_SPRITE, (self.x, self.y))
 
+        # In Debug Mode, draw visible collision and coordinates
         if DEBUG_MODE:
             win.blit(
                 DEBUG_FONT.render(
@@ -167,6 +175,7 @@ class Platform:
             surface.fill((255, 0, 25))
             win.blit(surface, (self.x, self.y))
 
+# Draw objects in window
 def draw_window(win, players, platforms, score):
     win.blit(BG_SPRITE, (0, 0))
 
@@ -183,10 +192,12 @@ def draw_window(win, players, platforms, score):
 
     pygame.display.update()
 
+# Generate initial Platforms
 def generateInitialPlatforms():
     prev_y = FIELD_MARGIN
     platforms = []
 
+    # Make sure each platform has enough vertical spacing
     for i in range(MAX_PLATFORMS):
         x = random.randrange(
             FIELD_MARGIN,
@@ -199,11 +210,13 @@ def generateInitialPlatforms():
 
     return platforms
 
+# Main Function
 def main(genomes, config):
     networks = []
     ge = []
     players = []
 
+    # Set Neural Networks
     for _, g in genomes:
         network = neat.nn.FeedForwardNetwork.create(g, config)
         networks.append(network)
@@ -229,6 +242,7 @@ def main(genomes, config):
                 pygame.quit()
                 quit()
 
+        # Stop Generation when there are no players left
         if len(players) <= 0:
             run = False
             break
@@ -262,10 +276,8 @@ def main(genomes, config):
             # Move Player based on Neural Network Ouput
             if output[0] > 0.5:
                 player.moveLeft()
-            elif output[1] > 0.5:
+            elif output[0] < -0.5:
                 player.moveRight()
-            #else:
-                #player.resetMove()
 
             # Move Platforms if Player Y is above Jump Threshold
             if player.y <= JUMP_THRESHOLD:
@@ -274,30 +286,33 @@ def main(genomes, config):
 
             # Check Player - Platform Collision
             if player.collide(platforms) and index < len(ge):
-                ge[index].fitness += 0.1
+                ge[index].fitness += 0.4
                 player.jump()
 
             # Player Death
             if player.y >= WINDOW_HEIGHT and index < len(ge):
-                ge[index].fitness -= 5
+                ge[index].fitness -= 10
                 players.pop(index)
                 networks.pop(index)
                 ge.pop(index)
 
+        # Increase fitness if player reaches new height
         if current_height > 1 and index < len(ge):
             ge[index].fitness += 0.2
 
-        if current_height > 5:
-            current_height = 5
+        if current_height > SCROLLING_VELOCITY:
+            current_height = SCROLLING_VELOCITY
 
         score += current_height
 
+        # Move platforms when Player reaches above Jump Threshold
         for platform in platforms:
             platform.move(current_height)
 
         draw_window(win, players, platforms, score)
         old_score = score
 
+# Run AI
 def run(config_path):
     config = neat.config.Config(
         neat.DefaultGenome,
@@ -313,6 +328,10 @@ def run(config_path):
 
     winner = p.run(main, MAX_GENERATIONS)
 
+    with open('winner', 'wb') as f:
+        pickle.dump(winner, f)
+
+# Set Config
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config-feedforward.txt")
